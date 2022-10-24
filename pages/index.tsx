da7fdc64a6c85d669561ee6cd98524740302311e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-24 06:54:33 +0100

    show data from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,69 @@
+import axios from "axios";
 import type { NextPage } from "next";
+import generateSignature from "../lib/generateSignature";
+import { useState, useEffect } from "react";
+import getUserImage from "../lib/getUserImage";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  const [users, setUsers] = useState([]);
+
+  const handleSearch = (e: any) => {
+    const APPID = process.env.NEXT_PUBLIC_UPRODIT_APPID;
+    const ENV = process.env.NEXT_PUBLIC_UPRODIT_ENV;
+    const API = process.env.NEXT_PUBLIC_UPRODIT_API;
+
+    if (!APPID || !ENV || !API) return;
+
+    const query = e.target.value;
+    const url = `https://${API}/v1/search/all?startIndex=0&maxResults=10&usecase=perso`;
+
+    axios
+      .get(url, {
+        headers: {
+          Authorization: generateSignature(APPID, ENV, url),
+        },
+      })
+      .then(res => {
+        setUsers(res.data);
+      });
+  };
+
+  useEffect(() => {
+    document.querySelectorAll(".user-image").forEach(async (image: any) => {
+      const imageData = await (await getUserImage(image.id)).data.b64Content;
+      image.src = imageData
+        ? "data:image/png;base64, " + imageData
+        : "https://picsum.photos/500";
+    });
+  }, [users]);
+
+  return (
+    <div>
+      <div className="w-full p-2">
+        <input
+          type="search"
+          onChange={handleSearch}
+          className="w-full h-12 font-bold text-center rounded-lg outline-none ring-gray-600 ring"
+        />
+      </div>
+
+      <div className="grid grid-cols-4 gap-8 bg-[#f3f5f8]">
+        {users.map((user: any) => (
+          <div key={user.id} className="bg-white rounded-lg ">
+            <img id={user.image_id} className="w-full rounded-lg user-image" />
+
+            <div className="flex flex-col gap-4 p-2">
+              <p className="w-full font-medium text-center">
+                {user.denomination}
+              </p>
+              <p className="w-full"> Specialized in :</p>
+              <p className="w-full">{user.specialities.join(" / ")}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
